70b458378145ca4e4c05236eb5a73eef59f23a34
Adriano <adr.hardtke@gmail.com>
2020-10-09 16:20:47 -0300

    Criando entity Post com user aninhado

--- src/main/java/com/adriano/workshopmongo/config/Instantiation.java
+++ src/main/java/com/adriano/workshopmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.adriano.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.adriano.workshopmongo.domain.Post;
 import com.adriano.workshopmongo.domain.User;
+import com.adriano.workshopmongo.repository.PostRepository;
 import com.adriano.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -15,18 +19,29 @@ public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		
-		userRepository.deleteAll();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
+		userRepository.deleteAll();
+		postRepository.deleteAll();
+		
+		/* Seed Users */
 		User userSeed1 = new User(null, "John Smith", "john@mail.com");
 		User userSeed2 = new User(null, "Joe Doe", "joe@mail.com");
 		User userSeed3 = new User(null, "Uncle Bob", "bob@mail.com");
-		
 		userRepository.saveAll(Arrays.asList(userSeed1, userSeed2, userSeed3));
 		
+		/* Seed Posts */
+		Post post1 = new Post(null,sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para Gramado!", userSeed1);
+		Post post2 = new Post(null,sdf.parse("23/03/2018"), "Bom dia!", "Tenha um bom dia.", userSeed1);
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 	
 }
