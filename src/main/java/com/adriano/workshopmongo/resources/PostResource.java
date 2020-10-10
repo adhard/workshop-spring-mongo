9d9cedc94faa57089bbbd70fe9682c3705968bbd
Adriano <adr.hardtke@gmail.com>
2020-10-10 11:59:34 -0300

    consulta com varios criterios

--- src/main/java/com/adriano/workshopmongo/resources/PostResource.java
+++ src/main/java/com/adriano/workshopmongo/resources/PostResource.java
@@ -1,25 +1,19 @@
 package com.adriano.workshopmongo.resources;
 
-import java.net.URI;
+import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.adriano.workshopmongo.domain.Post;
-import com.adriano.workshopmongo.domain.User;
-import com.adriano.workshopmongo.dto.UserDTO;
 import com.adriano.workshopmongo.resources.util.URL;
 import com.adriano.workshopmongo.services.PostService;
-import com.adriano.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value="/posts")
@@ -40,6 +34,21 @@ public class PostResource {
 		
 		text = URL.decodeParam(text);
 		List<Post> list = service.findByTitle(text);
+		
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@RequestMapping(value="/fullsearch", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate){
+		
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		
+		List<Post> list = service.fullSearch(text, min, max);
 		
 		return ResponseEntity.ok().body(list);
 	}
--- src/main/java/com/adriano/workshopmongo/services/PostService.java
+++ src/main/java/com/adriano/workshopmongo/services/PostService.java
@@ -1,18 +1,15 @@
 package com.adriano.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.adriano.workshopmongo.domain.Post;
-import com.adriano.workshopmongo.domain.User;
-import com.adriano.workshopmongo.dto.UserDTO;
 import com.adriano.workshopmongo.repository.PostRepository;
-import com.adriano.workshopmongo.repository.UserRepository;
 import com.adriano.workshopmongo.services.exception.ObjectNotFoundException;
-
-import java.util.Optional;
 
 @Service
 public class PostService {
@@ -29,6 +26,11 @@ public class PostService {
 	public List<Post> findByTitle(String text){
 		//return repo.findByTitleContainingIgnoreCase(text);
 		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000); // para adicionar +1 dia
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 	
 	
