cd34f4deff06f7465f24dd06bd7be200df62f1a5
Adriano <adr.hardtke@gmail.com>
2020-10-09 22:18:20 -0300

    acrescentando comentarios aos posts

--- src/main/java/com/adriano/workshopmongo/config/Instantiation.java
+++ src/main/java/com/adriano/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.adriano.workshopmongo.domain.Post;
 import com.adriano.workshopmongo.domain.User;
 import com.adriano.workshopmongo.dto.AuthorDTO;
+import com.adriano.workshopmongo.dto.CommentDTO;
 import com.adriano.workshopmongo.repository.PostRepository;
 import com.adriano.workshopmongo.repository.UserRepository;
 
@@ -42,8 +43,19 @@ public class Instantiation implements CommandLineRunner {
 		/* Seed Posts */
 		Post post1 = new Post(null,sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para Gramado!", new AuthorDTO(userSeed1));
 		Post post2 = new Post(null,sdf.parse("23/03/2018"), "Bom dia!", "Tenha um bom dia.", new AuthorDTO(userSeed1));
+		
+		/* Create Comments*/
+		CommentDTO c1 = new CommentDTO("Boa viagem!!", sdf.parse("21/03/2018"), new AuthorDTO(userSeed2));
+		CommentDTO c2 = new CommentDTO("Aproveite!!", sdf.parse("21/03/2018"), new AuthorDTO(userSeed3));
+		CommentDTO c3 = new CommentDTO("Tenha um bom dia!!", sdf.parse("21/03/2018"), new AuthorDTO(userSeed2));
+		
+		/* Insert Comments to posts */
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
+		/* Add Posts Array to User entity */
 		userSeed1.getPosts().addAll(Arrays.asList(post1, post2));
 		userRepository.save(userSeed1);
 	}
